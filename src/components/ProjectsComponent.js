74e378bd48a7711cdc75bdac8d77bd6dbba20953
MatteoOrlando <mat.orlando88@gmail.com>
2024-06-18 13:34:19 +0200

    update

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -3,7 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import { FaReact, FaNodeJs, FaGithub } from 'react-icons/fa';
 import { SiSpring, SiPostgresql } from 'react-icons/si';
 import image1 from '../assets/Img/img-project-1.png';
-import image2 from '../assets/Img/Immagine home-screen.png';
+import image2 from '../assets/Img/test-img.png';
 import image3 from '../assets/Img/Immagine catalogo.png';
 import image4 from '../assets/Img/Immagine dettagli prodotto.png';
 import image5 from '../assets/Img/Immagine cart.png';
@@ -11,34 +11,6 @@ import '../assets/Project.css';
 
 function ProjectsComponent() {
   const projects = [
-    {
-      title: 'Capstone Boot Camp',
-      description:
-        'Game Portal: mini e-commerce con sistema di login/carrello e gestione admin utilizzando Spring Security.',
-      images: [image1, image2, image3, image4, image5],
-      technologies: [
-        {
-          icon: <FaReact style={{ color: '#61DAFB', marginRight: '5px' }} />,
-          name: ' React',
-        },
-        {
-          icon: <FaNodeJs style={{ color: '#339933', marginRight: '5px' }} />,
-          name: ' Node.js',
-        },
-        {
-          icon: <SiSpring style={{ color: '#6DB33F', marginRight: '5px' }} />,
-          name: ' Spring',
-        },
-        {
-          icon: (
-            <SiPostgresql style={{ color: '#336791', marginRight: '5px' }} />
-          ),
-          name: 'PostgreSQL',
-        },
-      ],
-      repoUrl: 'https://github.com/yourusername/capstone-project',
-    },
-
     {
       title: 'Capstone Boot Camp',
       description:
@@ -94,11 +66,11 @@ function ProjectsComponent() {
             </div>
           </div>
           <Carousel>
-            {project.images.map((img, index) => (
+            {project.images.map((image, index) => (
               <Carousel.Item key={index}>
                 <div
-                  className="project-card"
-                  style={{ backgroundImage: `url(${img})` }}
+                  className="project-page-card"
+                  style={{ backgroundImage: `url(${image})` }}
                 ></div>
               </Carousel.Item>
             ))}
